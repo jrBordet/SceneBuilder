185cfae358ad48d28129905a43ea6062924dcdb6
Jean Raphaël Bordet <jr.bordet@synesthesia.it>
2020-05-21 22:12:05 +0200

    Initial commit

---
+++ SceneBuilder/SceneBuilder.h
@@ -0,0 +1,19 @@
+//
+//  SceneBuilder.h
+//  SceneBuilder
+//
+//  Created by Jean Raphael Bordet on 21/05/2020.
+//  Copyright © 2020 Jean Raphael Bordet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SceneBuilder.
+FOUNDATION_EXPORT double SceneBuilderVersionNumber;
+
+//! Project version string for SceneBuilder.
+FOUNDATION_EXPORT const unsigned char SceneBuilderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SceneBuilder/PublicHeader.h>
+
+
